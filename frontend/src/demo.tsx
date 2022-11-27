ef99945454a719ef98ac5bdb07f032f3b4248f56
Luka Huang <szteven3@gmail.com>
2022-11-28 04:25:56 +0800

    implement table

--- frontend/src/demo.tsx
+++ frontend/src/demo.tsx
@@ -1,48 +1,76 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'tag', headerName: '分組', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'page_view',
     type: 'number',
-    width: 90,
+    headerName: '瀏覽數',
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'follower_number',
+    type: 'number',
+    headerName: '追蹤數',
+    width: 90
   },
+  // { field: 'id', headerName: 'id', width: 70 },
+  { field: 'title', headerName: 'Topic', width: 200 },
+  { field: 'author_name', headerName: 'Name', width: 120 },
+  { field: 'description', headerName: 'Description', width: 1000 },
+  
+  {
+    field: 'link',
+    type: 'text',
+    headerName: 'Link',
+    width: 420,
+  },
+  
+  
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+interface IIronman {
+  id: number;
+  author_name: string;
+  description: string;
+  follower_number: number; 
+  link: string;
+  page_view: number;
+  progress: number;
+  tag: string;
+  title: string;
+}
+
+const useFetch = () => {
+  const [data, setData] = useState<IIronman[]>([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://winter-darkness-4385.fly.dev/ironman2022');
+      const json = await response.json();console.log(json['ironman_list'])
+      setData(json['ironman_list'])
+    }
+    fetchData();
+  }, []);
+
+  return data;
+};
 
 export default function DataTable() {
+  const result = useFetch();
   return (
-    <div style={{ height: 800, width: '100%' }}>
+    <div style={{ width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={result}
         columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
+        pageSize={20}
+        rowsPerPageOptions={[50]}
+        autoHeight={true}
+        disableColumnSelector={false}
+        disableExtendRowFullWidth={false}
+
       />
     </div>
   );
 }
+
+
