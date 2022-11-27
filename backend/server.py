0f285f8f23005d2b31e4f34f3e9dd2bb8b04f50d
Luka Huang <szteven3@gmail.com>
2022-11-27 23:06:11 +0800

    implement mongo for backend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI')
+COLLECTION_NAME = "2022_ironman"
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = f"{MONGO_URI}"
+mongo = PyMongo(app)
+collection = mongo.db.get_collection(COLLECTION_NAME)
+print(list(collection.find()))
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello():
+    return render_template('hello.html')
 
+
+@app.route('/ironman2022')
+def ironman2022():
+    ironman_list = list(collection.find())
+    return jsonify({"ironman_list": ironman_list})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- crawler/mongo_author.py
+++ crawler/mongo_author.py
@@ -28,16 +28,16 @@ def get_ironman_list(collection):
     return list(collection.find())
 
 
-# def main():
-#     collection = init_mongo()
-#     author_dict = {
-#         'author_name': 'jddj42840', 'title': '資工琪琪的後端學習筆記(python&flask)',
-#         'link': 'https://ithelp.ithome.com.tw/users/20150282/ironman/5135',
-#         'description': '嗨摟~我是資工琪琪現在正在學習利用flask與python開發網頁後端，希望再未來的三十天大家可以與琪琪一同勤奮的學習吧~~~',
-#         'progress': 100, 'tag': 'Modern Web'
-#     }
-#     add_ironman(author_dict, collection)
-#     print(get_ironman_list(collection))
-#
-#
-# main()
+def main():
+    collection = init_mongo()
+    # author_dict = {
+    #     'author_name': 'jddj42840', 'title': '資工琪琪的後端學習筆記(python&flask)',
+    #     'link': 'https://ithelp.ithome.com.tw/users/20150282/ironman/5135',
+    #     'description': '嗨摟~我是資工琪琪現在正在學習利用flask與python開發網頁後端，希望再未來的三十天大家可以與琪琪一同勤奮的學習吧~~~',
+    #     'progress': 100, 'tag': 'Modern Web'
+    # }
+    # add_ironman(author_dict, collection)
+    print(get_ironman_list(collection))
+
+
+main()
